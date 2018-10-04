cc4b85c100cf80aa55266bedce28b9be2f1914cf
Liv <olivia.sart@hotmail.com>
2018-10-04 22:45:39 +0200

    Added onClick func to button picked up txt from EditText Created AlertDialogBox and show txt.

--- app/src/main/java/com/example/administrator/helloworld/MainActivity.java
+++ app/src/main/java/com/example/administrator/helloworld/MainActivity.java
@@ -1,21 +1,57 @@
 package com.example.administrator.helloworld;
 
+import android.content.SyncStatusObserver;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 //1.
+import android.widget.Button;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    //you want to declare display as a private instance var so its not only accesible in
+    private TextView txtView;
+    private EditText txtEdit;
+    private Button btn;
+
 
     @Override
+    // onCreate() -> método en el que se crea la actividad
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    //1.
-    public void sendMessage(View view) {
-        // Do something in response to button
-    }
 
+        txtView = findViewById(R.id.Name);
+        txtEdit = findViewById(R.id.EditName);
+        btn = findViewById(R.id.button);
+        //We create an instance of View.OnClickListener and wire it to the btn using setOnClickListener(View.OnClickListener). As a result**
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // **The system executes code here on the MAIN THREAD after user presses button
+
+                //We pick up the text in the Activity where the view hosts the EditText
+                String txt = txtView.getText().toString();
+                String input = txtEdit.getText().toString();
+
+                //1. Instantiate an AlertDialog.Builder with its constructor
+                AlertDialog.Builder builder= new AlertDialog.Builder(MainActivity.this);
+                //2.
+                builder.setTitle("Input Data");
+                builder.setMessage(txt +": "+ input);
+                //3. Get the AlertDialog from create()
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+                //txtEdit.setText(txt);
+                //txtView.setText(input);
+
+            }
+        });
+
+    }
 
 }
